ba2ad3e95af5e15f2b715a45dad08a5cfe8a0e81
zsefvcx <zsefvcx@list.ru>
2023-05-24 21:00:39 +0300

    06_Layout text field styled done

--- 06_Layout/text_field_styled/lib/main.dart
+++ 06_Layout/text_field_styled/lib/main.dart
@@ -14,7 +14,15 @@ class MyApp extends StatelessWidget {
       debugShowMaterialGrid: false,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrangeAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.tealAccent),
+        inputDecorationTheme: const InputDecorationTheme(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 3, color: Colors.amberAccent),
+          ),
+        ),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Text Field Styled'),
@@ -34,6 +42,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   final _fieldText = TextEditingController();
+  final _fieldText2 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -43,36 +52,61 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(10.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children:
-            [
-              TextField(
-                controller: _fieldText,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                    gapPadding: 5,
-                  ),
-                  hintText: 'Введите значение',
-                  hintStyle: TextStyle(fontSize: 22),
-                  helperText: 'Поле для поиска заметок',
-                  suffixIcon: Icon(Icons.search),
-                  filled: true,
-                  iconColor: Colors.purple,
-                  label: Text('  Search  ',style: TextStyle(fontSize: 22)),
-                ),
-                style: const TextStyle(fontSize: 22),
-                onSubmitted: (value) {
-                  print(value);
-                  _fieldText.clear();
-                },
-              ),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
 
-            ],
+              children:
+              [
+                TextField(
+                  controller: _fieldText,
+
+                  decoration: const InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 3, color: Colors.purple),
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 3, color: Colors.purple),
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    ),
+                    hintText: 'Введите значение',
+                    hintStyle: TextStyle(fontSize: 22),
+                    helperText: 'Поле для поиска заметок',
+                    suffixIcon: Icon(Icons.search),
+                    filled: true,
+                    suffixIconColor: Colors.purple,
+                    label: Text('  Search  ',style: TextStyle(fontSize: 22)),
+                  ),
+                  style: const TextStyle(fontSize: 22),
+                  onSubmitted: (value) {
+                    print(value);
+                    //_fieldText.clear();
+                  },
+                ),
+                const Divider(height: 50,),
+                TextField(
+                  controller: _fieldText2,
+                  decoration: const InputDecoration(
+                    hintText: 'Введите значение',
+                    hintStyle: TextStyle(fontSize: 22),
+                    helperText: 'Поле для поиска заметок',
+                    suffixIcon: Icon(Icons.search),
+                    filled: true,
+                    suffixIconColor: Colors.purple,
+                    label: Text('  Search  ',style: TextStyle(fontSize: 22)),
+                  ),
+                  style: const TextStyle(fontSize: 22),
+                  onSubmitted: (value) {
+                    print(value);
+                    //_fieldText2.clear();
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
