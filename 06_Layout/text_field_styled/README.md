172ec815b077d336102780e73dd45f7a5a30fe7b
zsefvcx <zsefvcx@list.ru>
2023-05-24 16:20:30 +0300

    06_Layout text field styled init

---
+++ 06_Layout/text_field_styled/README.md
@@ -0,0 +1,7 @@
+# text_field_styled
+
+text_field_styled
+
+## Getting Started
+
+text_field_styled
---
+++ 06_Layout/text_field_styled/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Text Field Styled',
+      debugShowMaterialGrid: false,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Text Field Styled'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Text Field Styled',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
