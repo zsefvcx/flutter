a745fb3cc0466a12b09671747823200d491f1f57
zsefvcx <zsefvcx@list.ru>
2023-06-02 12:01:08 +0300

    10 Testing 20%

--- 10_Testing/testing/test/widget_test.dart
+++ 10_Testing/testing/test/widget_test.dart
@@ -30,7 +30,8 @@ void main() {
       expect(find.text('Войти',findRichText: true), findsOneWidget);
       expect(find.text('Регистрация',findRichText: true), findsNothing);
 
-      //2. Тест что поля изначально пустые и пытаемся залогиниться
+      //Email и Phone
+      //2. Тест начального состояния что автоматически не лоиниться с пустыми полями
       expect(tester.widget<TextFormField>(find.byKey(Key('textEmailField'))).initialValue, '');
       expect(tester.widget<TextFormField>(find.byKey(Key('textPhoneField'))).initialValue, '');
       await tester.tap(find.byKey(Key('buttonSendField')));
@@ -44,21 +45,59 @@ void main() {
       expect( find.text('Введите телефон', findRichText: true), findsOneWidget);
       expect((tester.firstWidget(find.text('Введите телефон', findRichText: true)) as RichText).text.style!.color, Color(0xffd32f2f));
 
-      //3. Вводим неправильное мыло и пытаемся залогиниться (бе)
-      expect(tester.widget<TextFormField>(find.byKey(Key('textEmailField'))).initialValue, '');
-      expect(tester.widget<TextFormField>(find.byKey(Key('textPhoneField'))).initialValue, '');
-      await tester.enterText(find.byKey(Key('textEmailField')), 'testtest.tes');
-      await tester.tap(find.byKey(Key('buttonSendField')));
-      await tester.pumpAndSettle();
-      expect( find.text('Email',findRichText: true), findsOneWidget);
-      expect((tester.firstWidget(find.text('Email',findRichText: true)) as RichText).text.style!.color, Color(0xffd32f2f));
-      expect( find.text('Введите email', findRichText: true), findsOneWidget);
-      expect((tester.firstWidget(find.text('Введите email', findRichText: true)) as RichText).text.style!.color, Color(0xffd32f2f));
-      expect( find.text('Phone',findRichText: true), findsOneWidget);
-      expect((tester.firstWidget(find.text('Phone',findRichText: true)) as RichText).text.style!.color, Color(0x99000000));
-      expect( find.text('Введите телефон', findRichText: true), findsOneWidget);
-      expect((tester.firstWidget(find.text('Введите телефон', findRichText: true)) as RichText).text.style!.color, Color(0xffd32f2f));
+      //2 Проверка валидного адреса
+      //2.1 Email
 
+
+
+
+
+
+
+
+      //3 Проверка на невалидность
+      //3.1
+
+      //3.2 Email
+      Future<void> TestWidgetEmail(WidgetTester tester, String email) async{
+        print(email);
+        expect(tester.widget<TextFormField>(find.byKey(Key('textEmailField'))).initialValue, '');
+        expect(tester.widget<TextFormField>(find.byKey(Key('textPhoneField'))).initialValue, '');
+        await tester.enterText(find.byKey(Key('textEmailField')), email);
+        expect(find.text(email), findsOneWidget);
+        await tester.tap(find.byKey(Key('buttonSendField')));
+        await tester.pumpAndSettle();
+        expect( find.text('Email',findRichText: true), findsOneWidget);
+        expect((tester.firstWidget(find.text('Email',findRichText: true)) as RichText).text.style!.color, Color(0xffd32f2f));
+        expect( find.text('Поле email заполнено не корректно', findRichText: true), findsOneWidget);
+        expect((tester.firstWidget(find.text('Поле email заполнено не корректно', findRichText: true)) as RichText).text.style!.color, Color(0xffd32f2f));
+        expect( find.text('Phone',findRichText: true), findsOneWidget);
+        expect((tester.firstWidget(find.text('Phone',findRichText: true)) as RichText).text.style!.color, Color(0x99000000));
+        expect( find.text('Введите телефон', findRichText: true), findsOneWidget);
+        expect((tester.firstWidget(find.text('Введите телефон', findRichText: true)) as RichText).text.style!.color, Color(0xffd32f2f));
+      }
+      //3.2.1 Отсутствие @ в email
+      await TestWidgetEmail(tester, 'testtest.tst');
+      //3.2.2 Отсутствие локальной части
+      await TestWidgetEmail(tester, '@test.tst');
+      //3.2.3 Отсутствие доменной части
+      await TestWidgetEmail(tester, 'test@');
+      //3.2.4 Содержит две точки подряд
+      await TestWidgetEmail(tester, 'test@test..ru');
+      //3.2.5 Локальная часть начинается с . (точки)
+      await TestWidgetEmail(tester, '.test@test.ru');
+      //3.2.6 Доменная часть начинается с . (точки)
+      await TestWidgetEmail(tester, 'test@.test.ru');
+      //менять валидатор с мылом
+      //3.2.6 Превышение длины локальной части (максимальная допустимая 64 символа)
+      //String text = List<String>.generate(65, (index) => 'a').join('');
+      //await TestWidgetEmail(tester, '$text@test.ru');
+      //3.2.7 Превышение длины доменного имени (максимальная допустимая 255 символов)
+      //text = List<String>.generate(256, (index) => 'a').join('');
+      //await TestWidgetEmail(tester, 'test@$text.ru');
+      //3.2.8 Превышение длины участка доменного имени между точками (максимальная допустимая 63 символа)
+      //text = List<String>.generate(64, (index) => 'a').join('');
+      //await TestWidgetEmail(tester, 'test@test$text.ru');
 
       //
       // await tester.enterText(find.byKey(Key('textPhoneField')), '');
